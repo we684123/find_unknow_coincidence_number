b3896a7dc499641cbefe26aa891ae54020576022
永格天 <we684123@gmail.com>
2020-06-03 18:48:24 +0800

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # find_unknow_coincidence_number
-山外山+青龍山=青龍山外 
+山外山+青龍山=青龍山外    
+請問 "青"、"龍"、"山"、"外" 個別代表什麼數字?   
+
+ans:    
+![Imgur](https://imgur.com/3GrPgc3.png)    
+
+某天在逛偷偷說的時候遇到的題目，無聊就解一下。    
+原題目:    
+![plurk_image](https://images.plurk.com/5U3lo8XyewAVhfHugyWbEu.jpg)    
---
+++ main.py
@@ -0,0 +1,90 @@
+import re
+
+# 下面是我誤會題目意思做出來的廢品QQ
+# def t(text):
+#     i = []
+#     for x in text:
+#         if x not in i:
+#             i.append(x)
+#     ans = ''
+#     for j in i:
+#         ans += j
+#     return ans
+#
+#
+# a = '山外山青龍山'
+# print(t(a))
+# # ------
+
+
+def compare(add=[], minus=[], equal=''):
+    if type(equal) != type([]):
+        raise 'equal need is []'
+    left = 0
+    right = len(equal)
+    for i in add:
+        left += int(i)
+    for i in minus:
+        left -= int(i)
+    if left == int(equal[0]):
+        return True
+    else:
+        return False
+
+
+def renew_map(u_len, unknown, now_tag):
+    map_ed = {}
+    for i in range(0, u_len):
+        map_ed[unknown[i]] = now_tag[i]
+    return map_ed
+
+
+# text_list = equal
+# text_list
+
+def fm3294(text_list, map_ed):
+    # if text_list == []:
+    #     return []
+    for i in range(0, len(text_list)):
+        # print(text_list[i])
+        for k in map_ed:
+            # print(k)
+            if k in text_list[i]:
+                # r = re.compile(f'{k}')
+                text_list[i] = re.sub(k, map_ed[k], text_list[i])
+                # print(text_list[i])
+    return text_list
+
+
+# 這裡改三個變數內容
+add = ['山外山', '青龍山']
+minus = []
+equal = ['青龍山外']
+
+
+k = add.copy()
+k.extend(minus)
+k.extend(equal)
+unknown = []
+for i in k:
+    for j in i:
+        if j not in unknown:
+            unknown.append(j)
+        # print(j)
+u_len = len(unknown)
+
+# ----
+max_m0 = int(u_len * '9')
+for i in range(0, max_m0 + 1):
+    # for i in range(0, 5):
+    #     # print(i)
+    now_tag = list('{:0>4d}'.format(i))
+    map_ed = renew_map(u_len, unknown, now_tag)
+    add_ed = fm3294(add.copy(), map_ed)
+    minus_ed = fm3294(minus.copy(), map_ed)
+    equal_ed = fm3294(equal.copy(), map_ed)
+    # 取代完成後開始排左右--------
+    if compare(add_ed, minus_ed, equal_ed):
+        print(f'add = {add_ed}')
+        print(f'minus = {minus_ed}')
+        print(f'equal = {equal_ed}')
